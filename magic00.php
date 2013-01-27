9dd36c6c4dadeb84fe6c8769db0880c0da8f8927
Steven Senkus <steven.senkus@yahoo.com>
2013-01-27 08:39:09 -0800

    Added the files for this monstrosity
    The font was from dafont and most of the effects were copypasted from
    somewhere online with a little styling thrown in!

---
+++ magic00.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles.css" />						
+	</head>
+	<body>
+		<ul class="horiz_menu">
+			<li><a id="l0" href="#">item 0</a></li>
+			<li><a id="l1" href="#">item 1</a></li>
+			<li><a id="l2" href="#">item 2</a></li>
+			<li><a id="l3" href="#">item 3</a></li>
+			<li><a id="l4" href="#">item 4</a></li>
+			<li><a id="l5" href="#">item 5</a></li>
+			<li><a id="l6" href="#">item 6</a></li>
+			<li><a id="l7" href="#">item 7</a></li>
+			<li><a id="l8" href="#">item 8</a></li>
+			<li><a id="l9" href="#">item 9</a></li>	
+		</ul>
+		<br />
+		<div id="outer" >
+			<div id="inner" >		
+				<p>another great seizure-inducing css3 masterpiece!</p>
+			</div>
+		</div>
+		<p id="cf3" style="color:rgb(255,255,255); margin:20px;">what up</p>
+	</body>
+</html> 
